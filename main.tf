88fa2f005b9154c99e1c6e7d00cee878f742c5da
Pablo Dantas <84293912+pablodantas-ti@users.noreply.github.com>
2026-05-08 20:30:17 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "terraform_vpc" {
 
 # Correcao primeira issue
 resource "aws_flow_log" "example" {
-  log_destination      = "arn:aws:s3:::clc15-vini-terraform"
+  log_destination      = "arn:aws:s3:::clc15-pablo-terraform"
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.terraform_vpc.id
